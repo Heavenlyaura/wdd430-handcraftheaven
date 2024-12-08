9a94302947515c129620a2662ec531511b5c044f
heavenlyAura <goodnessokafor462@gmial.com>
2024-12-08 15:53:07 +0100

    `Refactor trending list layout and add promotional content`

--- src/components/trending/trending-list.tsx
+++ src/components/trending/trending-list.tsx
@@ -25,7 +25,7 @@ export default async function TrendingProducts() {
         <h3 className="font-light">TRENDING</h3>
         <h4 className="font-bold">Shop our Popular HandCrafted Items</h4>
       </div>
-      <div className="flex justify-evenly mb-20">
+      <div className="flex flex-wrap justify-evenly mb-20">
         {trendingProducts.map((product) => (
           <div key={product.productId} className="flex flex-col gap-4">
             <Image
@@ -45,8 +45,21 @@ export default async function TrendingProducts() {
           </div>
         ))}
       </div>
-      <div className="max-w-[1280px] m-auto">
-        <Image src={imageTypeLayer.Icon} alt={imageTypeLayer.alt} />
+      <div className="grid grid-cols-2 grid-rows-1 ">
+        <div className="max-w-[1180px] m-auto col-start-1 col-end-3 row-start-1 row-end-2">
+          <Image src={imageTypeLayer.Icon} alt={imageTypeLayer.alt} />
+        </div>
+        <div className="flex flex-col fred col-start-1 col-end-2 row-start-1 row-end justify-center items-center">
+          <div className="flex flex-col gap-4 text-white w-[355px] h-[200px] p-4">
+            <h4 className="font-bold text-5xl">
+              <span className="bg-black px-2">35%</span> OFF
+            </h4>
+            <p className="text-2xl font-bold">
+              Get the best for less this Christmas season!🎄
+            </p>
+            <p>#HEAVENLYAURAEXPERIENCE</p>
+          </div>
+        </div>
       </div>
     </div>
   );
