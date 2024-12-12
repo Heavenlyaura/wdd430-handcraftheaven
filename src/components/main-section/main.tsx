a38cd3e4653674afda4101e7b11d1a67c7fc8115
heavenlyAura <goodnessokafor462@gmail.com>
2024-12-12 14:31:24 +0100

    `Update banner image component and main section layout`

--- src/components/main-section/main.tsx
+++ src/components/main-section/main.tsx
@@ -1,11 +1,13 @@
 import TrendingProducts from "./trending";
 import BannerImage from "./banner-img";
+import DisplayWrapedImages from "../trending/wrap-img";
 
 export default function MainSection() {
   return (
     <>
       <TrendingProducts />
       <BannerImage />
+      <DisplayWrapedImages/>
     </>
   );
 }
