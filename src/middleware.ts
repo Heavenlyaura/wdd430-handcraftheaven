ea37378b49a931378c500c7b3c04a693c9d4c816
heavenlyAura <goodnessokafor462@gmail.com>
2024-12-16 00:50:04 +0100

    `Refactor middleware function: remove unused code and reorganize logic`

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,23 +9,21 @@ export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
+
   const cookie = (await cookies()).get("session")?.value;
 
-  if (cookie) {
-    try {
-      const session = await decrypt(cookie);
-      if (isProtectedRoute && !session?.user) {
-        return NextResponse.redirect(new URL("/login", req.nextUrl));
-      }
-
-      if (isPublicRoute && session?.user) {
-        return NextResponse.redirect(new URL("/", req.nextUrl));
-      }
-    } catch (error) {
-      console.error("Error decrypting session cookie:", error);
+  try {
+    const session = await decrypt(cookie);
+    console.log("Session", session);
+    if (isProtectedRoute && !session?.user) {
+      return NextResponse.redirect(new URL("/login", req.nextUrl));
     }
-  } else {
-    console.log("No session cookie found.");
+
+    if (isPublicRoute && session?.user) {
+      return NextResponse.redirect(new URL("/", req.nextUrl));
+    }
+  } catch (error) {
+    console.error("Error decrypting session cookie:", error);
   }
 
   return NextResponse.next();
