e70b65759200c5828aa6b536153525488823dafb
heavenlyAura <goodnessokafor462@gmail.com>
2024-12-17 15:57:50 +0100

    `feat: updated session encryption and decryption logic, added role-based route protection`

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,23 +3,30 @@ import { NextRequest, NextResponse } from "next/server";
 import { decrypt } from "./library/session";
 
 const protectedRoutes = ["/profile"];
+const roleProtectedRoutes = ["/profile/products"];
 const publicRoutes = ["/login", "/register"];
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
+  const isRoleProtectedRoute = roleProtectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
 
   const cookie = (await cookies()).get("session")?.value;
 
   try {
     const session = await decrypt(cookie);
+
     if (isProtectedRoute && !session?.user) {
       return NextResponse.redirect(new URL("/login", req.nextUrl));
     }
 
-    if (isPublicRoute && session?.user) {
+    if (isPublicRoute && session?.user.role) {
       return NextResponse.redirect(new URL("/", req.nextUrl));
+    }
+
+    if (isRoleProtectedRoute && session?.user?.role !== "seller") {
+      return NextResponse.redirect(new URL("/profile", req.nextUrl));
     }
   } catch (error) {
     console.error("Error decrypting session cookie:", error);
