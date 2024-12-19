c698ccd97d3ea27690181e66dc77c97c51b833d2
heavenlyAura <goodnessokafor462@gmail.com>
2024-12-19 02:57:59 +0100

    `Update import statements and image array in DisplayWrapedImages component`

--- src/components/main-section/wrap-img.tsx
+++ src/components/main-section/wrap-img.tsx
@@ -1,21 +1,22 @@
-import contentImg1 from "/public/other/Contentimg-1.png";
-import contentImg2 from "/public/other/Contentimg-2.png";
 import wrpimg1 from "/public/other/wrapimg-1.jpg";
 import wrpimg2 from "/public/other/wrapimg-2.jpg";
+import wrpimg3 from "/public/other/wrapimg-3.jpg";
+import wrpimg4 from "/public/other/wrapimg-4.jpg";
+
 import Image from "next/image";
 
 export default function DisplayWrapedImages() {
-  const imgArray = [wrpimg1, contentImg1, wrpimg2, contentImg2];
+  const imgArray = [wrpimg1, wrpimg3, wrpimg2, wrpimg4];
 
   return (
-    <div className="flex justify-center items-center bg-[#ECDFDB] mt-10">
-      <div className="flex flex-wrap justify-center gap-10 p-4 border">
+    <div className="flex justify-center items-center bg-[#ECDFDB] mt-10 py-10">
+      <div className="flex flex-wrap justify-center gap-10 p-4">
         {imgArray.map((img, index) => (
           <Image
             key={index}
             src={img}
             alt={`Wrapped image ${index + 1}`}
-            className="w-[514px] md:h-[593px] object-contain"
+            className="w-[514px] md:h-[593px] object-cover"
           />
         ))}
       </div>
